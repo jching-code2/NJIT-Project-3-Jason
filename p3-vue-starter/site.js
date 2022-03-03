f4edfd2191b9054f4d668cd956c59e7aeb4cb552
jching-code2 <222jching@frhsd.com>
2022-03-03 11:18:53 -0500

    cs

--- p3-vue-starter/site.js
+++ p3-vue-starter/site.js
@@ -31,17 +31,81 @@ const vue_app = new Vue({
             })
       },
       data: {
+            title: "IMDB + David Top's 8 Movies",
+            owner: "David Kim",
+            github: "https://github.com/Davidshk29/project3is219",
             // This holds your movies.json data.
             movies: []
 
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            var title = "IMBD Jason's Top 8 Movies";
-            var owner ="Jason"
-
-            var gitHub ="https://jching-code2.github.io/NJIT-Project-3-Jason/"
-
       },
       methods: {
+            makeTextDate: function (dataArray) {
+                  let year = dataArray[0];
+                  let day = dataArray[2];
+                  let month = dataArray[1];
+
+                  switch (month) {
+                        case 1:
+                              month = 'January';
+                              break;
+                        case 2:
+                              month = 'February';
+                              break;
+                        case 3:
+                              month = 'March';
+                              break;
+                        case 4:
+                              month = 'April';
+                              break;
+                        case 5:
+                              month = 'May';
+                              break;
+                        case 6:
+                              month = 'June';
+                              break;
+                        case 7:
+                              month = 'July';
+                              break;
+                        case 8:
+                              month = 'August';
+                              break;
+                        case 9:
+                              month = 'September';
+                              break;
+                        case 10:
+                              month = 'October';
+                              break;
+                        case 11:
+                              month = 'November';
+                              break;
+                        case 12:
+                              month = 'December';
+                              break;
+                  }
+                  return ''.concat(month, ' ', day, ', ', year);
+            },
+            posterClick: function(index){
+                  if(this.movies[index].posterindex < (this.movies[index].posters.length -1)){
+                        this.movies[index].posterindex +=1;
+
+                  } else {
+                        this.movies[index].posterindex = 0;
+                  }
+            },
+
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            like: function (index) {
+                  this.movies[index].likes +=1;
+            },
+
+            dislike: function (index) {
+                  this.movies[index].dislikes +=1;
+            },
+
+            timeText: function (minutes) {
+                  return Math.floor(minutes / 60) + 'h '+ minutes % 60 + 'm';
+            }
       }
 })
+	
